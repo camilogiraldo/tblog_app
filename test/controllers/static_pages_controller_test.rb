1eb2d92ac1952d99646755cf7ed77d8dbe0db355
Camilo Giraldo <camilogiraldo91@gmail.com>
2016-09-23 21:47:35 +0100

    Add static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,14 +1,29 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  
+  test "should get root" do
+    get root_url
+    assert_response :success
+  end
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Home | TBlog app Home page"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+    assert_select "title", "Help | TBlog app Help page"
+  end
+
+  test "should get about" do
+  	get static_pages_about_url
+  	assert_response :success
+  	assert_select "title", "About | TBlog app About page"
   end
 
 end
+ 
