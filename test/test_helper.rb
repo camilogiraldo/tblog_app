ed1c67fcac9447182f6a9b4f0e1fc304c552312c
Camilo Giraldo <camilogiraldo91@gmail.com>
2016-10-07 22:07:27 +0100

    Authentication and page protection implementation. Friendly fordwaring added too

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,12 +7,13 @@ Minitest::Reporters.use!
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
+  
   def is_logged_in?
   	!session[:user_id].nil?
   end
   # Add more helper methods to be used by all tests here...
   def log_in_as(user)
-  	session[:user_id] = user.user_id
+  	session[:user_id] = user.id
   end
 end
 
