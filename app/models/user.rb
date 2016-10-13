f15b8607ad01965f9453f08949b46d51e01ecbec
Camilo Giraldo <camilogiraldo91@gmail.com>
2016-10-13 22:41:31 +0100

    Add migration for signup confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
 
-	attr_accessor :remember_token
+	attr_accessor :remember_token, :activation_token
+	before_save :downcase_email
+	before_create :create_activation_digest
 
-
-	before_save { self.email = email.downcase }
+	#before_save { self.email = email.downcase }
 	validates :name, presence: true, length: {maximum: 50}
 
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
@@ -24,6 +25,7 @@ class User < ApplicationRecord
 		SecureRandom.urlsafe_base64
 	end
 
+
 	def remember
 		self.remember_token = User.new_token
 		update_attribute(:remember_digest, User.digest(remember_token))
@@ -37,4 +39,16 @@ class User < ApplicationRecord
 		return false if remember_digest.nil?
 		BCrypt::Password.new(remember_digest).is_password?(remember_token)
 	end
+
+	private
+
+		def downcase_email
+			self.email = email.downcase
+		end
+
+		def create_activation_digest
+			self.activation_token = User.new_token
+			self.activation_digest = User.digest(activation_token)
+		end
+
 end
