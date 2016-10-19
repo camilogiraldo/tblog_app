64ceb8595ca4aa51e4817358092fab54f18961eb
Camilo Giraldo Ch <camilogiraldo91@gmail.com>
2016-10-19 15:36:15 -0500

    Add micropost model

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+	  # Returns the full title on a per-page basis.
+	  #FIXME
+  def full_title(page_title = '')
+    base_title = "Tblog app page"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
